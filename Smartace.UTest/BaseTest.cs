9fc5c62d347d60a6ba1abbd03d0054854bac07ee
Toochukwu Agbo <toochukwu@smartace.ng>
2021-02-08 22:13:41 +0100

    test

---
+++ Smartace.UTest/BaseTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Smartace.Application;
+using System.Reflection;
+using MediatR;
+
+
+namespace Smartace.UTest
+{
+    public class BaseTest
+    {
+        protected IServiceCollection GetCollection()
+        {
+            IServiceCollection service = new ServiceCollection();
+           
+            AppBootstraper.InitServices(service);
+            service.AddLogging();
+            service.AddMediatR(Assembly.GetExecutingAssembly());
+
+
+            return service;
+        }
+    }
+}
